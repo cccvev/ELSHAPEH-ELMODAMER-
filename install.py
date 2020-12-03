f3c3398a90e2ad3417dbec3a8c7f472433ca261a
cccvev <75307540+cccvev@users.noreply.github.com>
2020-12-03 15:06:49 +0200

    Add files via upload

---
+++ install.py
@@ -0,0 +1,14 @@
+import os
+os.system("git clone https://github.com/cccvev/ELSHAPEH-ELMODAMER")
+os.system ("""
+mv ELSHAPEH-ELMODAMER√ $HOME
+mv ELSHAPEH-ELMODAMER $HOME
+cd ELSHAPEH-ELMODAMER
+cd ELSHAPEH-ELMODAMER√
+mkdir .vmaker
+mv * /data/data/com.termux/files/home/ELSHAPEH-ELMODAMER√/.vmaker
+cd
+cd ELSHAPEH-ELMODAMER√
+python ELSHAPEH-ELMODAMER.py
+
+""")
